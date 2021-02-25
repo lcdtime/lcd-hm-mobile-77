553cbf269f90d1faccc16dcdbf256307466bd772
summerlcd <1273764460@qq.com>
2021-02-25 17:47:43 +0800

    第一次添加移动端练习lcd

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-icon name="chat-o" />
+    <van-icon name="https://b.yzcdn.cn/vant/icon-demo-1126.png" />
+    <van-switch v-model="checked" />
+    <div id="box">
+      <p>12312</p>
     </div>
+    <!-- 路由输出位置 -->
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      checked: true
     }
   }
 }
+</script>
+<style lang="sass">
+// scss 是新语法  sass是旧语法(旧语法书写不能加;不能加{} 和 styules类似)
+// scss 和 less类似,变量的定义($变量名),函数的定义有些不一样....
+// $color: green;
+// #app {
+//   #box {a
+//     width: 200px;
+//     height: 200px;
+//     background-color: pink;
+//     p {
+//       color: $color;
+//     }
+//   }
+// }
+// sass就写法
+// #app
+//   p
+//     background-color: pink
+//     width: 200px
+//     height: 200px
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//  Vant 支持一次性导入所有组件，引入所有组件会增加代码包体积，因此不推荐这种做法。
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+// Vue.use(Vant)
+// 按需导入如将并注册(自动导入yarn add babel-plugin-import -D)
+import { Button, Icon, Switch } from 'vant'
+// Vue.use(Button, Icon, Switch) // 错误的写法
+// 链式操作
+// Vue
+// .use(Button)
+// .use(Icon)
+// .use(Switch)
+Vue.use(Button)
+Vue.use(Icon)
+Vue.use(Switch)
 Vue.config.productionTip = false
 
 new Vue({
